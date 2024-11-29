ae02a76ff8f87b85077387fd4bd259a858767f40
AuliaZuifani <165286477+AuliaZuifani@users.noreply.github.com>
2024-11-29 11:05:47 +0700

    BooksController.php

--- app/controllers/BooksController.php
+++ app/controllers/BooksController.php
@@ -1,19 +1,17 @@
 <?php
-// app/controllers/UserController.php
+// app/controllers/BooksController.php
 require_once '../app/models/Books.php';
 
 class BooksController {
     private $bookModel;
 
     public function __construct() {
-        $this->bookModel = new Books();
+        $this->bookModel = new Books(); 
     }
-
 
     public function index() {
         $books = $this->bookModel->getAllBooks();
         require_once '../app/views/books/index.php';
-
     }
 
     public function create() {
@@ -22,45 +20,67 @@ class BooksController {
     }
 
     public function store() {
-        $id_buku= $_POST['id_buku'];
+        $id_buku = $_POST['id_buku'];
         $judul = $_POST['judul'];
-        $pengarang= $_POST['pengarang'];
-        $tahun= $_POST['tahun'];
-        $genre= $_POST['genre'];
-        $this->bookModel->add($id_buku, $judul, $pengarang, $tahun, $genre);
-        header('Location: /books/index');
-    }
-    // Show the edit form with the user data
-    public function edit($id_buku) {
-        $books = $this->bookModel->find($id_buku); // Assume find() gets user by ID
-        require_once __DIR__ . '/../views/books/edit.php';
-    }
+        $pengarang = $_POST['pengarang'];
+        $tahun = $_POST['tahun'];
+        $genre = $_POST['genre'];
+        $id_penerbit = $_POST['id_penerbit'];  
 
-    // Process the update request
-    public function update($id_buku) {
-        $data = [
-            'id_buku'=> $_POST['id_buku'],
-            'judul' => $_POST['judul'],
-            'pengarang' => $_POST['pengarang'],
-            'tahun' => $_POST['tahun'],
-            'genre' => $_POST['genre']
-        ];
-
-        $updated = $this->bookModel->update($id_buku, $data);
-        if ($updated) {
-            header("Location: /books/index"); // Redirect to user list
+        if (!empty($id_buku) && !empty($judul) && !empty($pengarang) && !empty($tahun) && !empty($genre) && !empty($id_penerbit)) {
+            $this->bookModel->add($id_buku, $judul, $pengarang, $tahun, $genre, $id_penerbit);
+            header('Location: /books/index');  
         } else {
-            echo "Failed to update books.";
+            echo "Semua data harus diisi!";
         }
     }
 
-    // Process delete request
+    public function edit($id)
+    {
+        $book = $this->bookModel->find($id);
+        if ($book) {
+            $books = $book; 
+            $penerbit = $this->bookModel->getAllPenerbit();  
+            require_once '../app/views/books/edit.php';
+        } else {
+            echo "Buku tidak ditemukan.";
+        }
+    }
+
+    public function update($id_buku) {
+        var_dump($_POST); 
+        if (isset($_POST['id_buku'])) {
+            $data = [
+                'id_buku' => $_POST['id_buku'],  
+                'judul' => $_POST['judul'],
+                'pengarang' => $_POST['pengarang'],
+                'tahun' => $_POST['tahun'],
+                'genre' => $_POST['genre'],
+                'id_penerbit' => $_POST['id_penerbit'],
+            ];
+    
+           
+            if (!empty($data['id_buku']) && !empty($data['judul']) && !empty($data['pengarang']) && !empty($data['tahun']) && !empty($data['genre']) && !empty($data['id_penerbit'])) {
+                $updated = $this->bookModel->update($id_buku, $data);
+                if ($updated) {
+                    header("Location: /books/index"); 
+                } else {
+                    echo "Gagal Menambah Buku.";
+                }
+            } else {
+                echo "Semua data harus diisi!";
+            }
+        } else {
+            echo "Buku tidak ditemukan!";
+        }
+    }
+    
     public function delete($id_buku) {
         $deleted = $this->bookModel->delete($id_buku);
         if ($deleted) {
-            header("Location: /books/index"); // Redirect to user list
+            header("Location: /books/index"); 
         } else {
-            echo "Failed to delete books.";
+            echo "Failed to delete book.";
         }
     }
 }
